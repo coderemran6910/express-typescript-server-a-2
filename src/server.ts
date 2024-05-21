7ca3f7fc2d3c96a80e5b0a792bdb5f5104a4a22b
Coder Emran <coderemran6910@gmail.com>
2024-05-21 22:53:43 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+
+async function main() {
+  try {
+    await mongoose.connect(config.db_url as string);
+
+    const port = process.env.PORT;
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main()
